d8ae86f51ac5567a8f5fe7b64a2c48c5c0a8a884
Douglas Eichelberger <dduugg@gmail.com>
2024-09-21 12:24:21 -0700

    Move remaining OS extensions to prepend

--- Library/Homebrew/os/mac/xcode.rb
+++ Library/Homebrew/os/mac/xcode.rb
@@ -229,7 +229,7 @@ module OS
 
       sig { returns(String) }
       def self.detect_version_from_clang_version
-        version = DevelopmentTools.clang_version
+        version = ::DevelopmentTools.clang_version
 
         return "dunno" if version.null?
 
